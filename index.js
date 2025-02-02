18dc27da61522d3633a39704a39f898512555f67
username_anjastabuni <reblytabuni2004@gmail.com>
2025-02-02 12:31:07 +0700

    mempraktekan middleware lebih lanjut

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use((req, res, next) => {
-  console.log("Middleware 1");
-  next();
-  console.log("middleware 1 after next");
-});
-app.use((req, res, next) => {
-  console.log("Middleware 2");
+  req.timeRequest = Date.now();
+  console.log(req.method, req.url);
   next();
 });
 
@@ -18,6 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/halaman", (req, res) => {
+  console.log(req.timeRequest);
   res.send("Hello halaman");
 });
 
