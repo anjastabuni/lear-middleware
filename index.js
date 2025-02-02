a9b027416ea79b8a7d88e90114a68aabccb28edc
username_anjastabuni <reblytabuni2004@gmail.com>
2025-02-02 12:08:11 +0700

    membuat middleware sendiri

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log("Middleware 1");
+  next();
+  console.log("middleware 1 after next");
+});
+app.use((req, res, next) => {
+  console.log("Middleware 2");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello Wordl");
