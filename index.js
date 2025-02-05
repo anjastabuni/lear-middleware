cbfd7dc02add24d97945bb8967b29c172f928f35
username_anjastabuni <reblytabuni2004@gmail.com>
2025-02-05 13:58:22 +0700

    membuat objek error class sendiri

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const ErrorHandler = require("./ErrorHandler");
 
 app.use(morgan("dev"));
 app.use((req, res, next) => {
@@ -15,7 +16,8 @@ const auth = (req, res, next) => {
     next();
   } else {
     // res.send("anda perlu masukan password");
-    throw new Error("anda perlu masukan password");
+    res.status(401);
+    throw new ErrorHandler("anda perlu masukan password", 401);
   }
 };
 
@@ -36,16 +38,25 @@ app.get("/admin", auth, (req, res) => {
   res.send("Hello admin");
 });
 
-app.use((req, res) => {
-  res.status(404).send("Page not found");
+// app.use((req, res) => {
+//   res.status(404).send("Page not found");
+// });
+
+app.get("/general/error", (req, res) => {
+  throw new ErrorHandler();
 });
 
 app.use((err, req, res, next) => {
-  console.log("*************************");
-  console.log("**********ERROR**********");
-  console.log("*************************");
-  next(err);
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).send(message);
 });
+
+// app.use((err, req, res, next) => {
+//   console.log("*************************");
+//   console.log("**********ERROR**********");
+//   console.log("*************************");
+//   next(err);
+// });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
